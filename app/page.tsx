e1c4fea8b0d65fe0bfb38f22ef2acb939aa6ebbb
florescente <campoflorescente@gmail.com>
2023-05-28 21:24:35 -0300

    chore: add tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home(): JSX.Element {
   return (
     <main>
-      <h1>Story of my life</h1>
+      <h1 className="text-3xl font-bold underline">Story of my life</h1>
     </main>
   )
 }
