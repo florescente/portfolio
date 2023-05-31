b57680f1d998bf78ca4d6050b94ca53b76780738
florescente <campoflorescente@gmail.com>
2023-05-31 18:13:24 -0300

    build: header design

--- components/header.tsx
+++ components/header.tsx
@@ -5,23 +5,61 @@ import Link from 'next/link'
 
 function header(): React.JSX.Element {
   return (
-    <ul className="flex flex-row justify-center gap-x-6 h-20 items-center">
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/about">About</Link>
-      </li>
-      <li>
-        <Link href="/projects">Projects</Link>
-      </li>
-      <li>
-        <Link href="/blog">Articles</Link>
-      </li>
-      <li>
-        <Link href="/contact">Contact</Link>
-      </li>
-    </ul>
+    <nav className="bg-amethyst border border-white flex justify-center">
+      <div className="flex flex-wrap items-center justify-between w-full max-w-screen-xl p-4">
+        <button
+          type="button"
+          className="flex md:hidden p-2 rounded-lg hover:bg-finn"
+        >
+          <svg
+            className="h-8 w-8 text-red-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </button>
+        <div>Sarah Nakada</div>
+        <div className="hidden md:block md:w-auto">
+          <ul className="flex flex-row justify-center gap-x-6 items-center p-3">
+            <li>
+              <Link href="/" className="p-3">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className="p-3">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="/projects" className="p-3">
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog" className="p-3">
+                Articles
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" className="p-3">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <button className="border rounded-full p-2 px-5 hover:bg-finn">
+          resume
+        </button>
+      </div>
+    </nav>
   )
 }
 
