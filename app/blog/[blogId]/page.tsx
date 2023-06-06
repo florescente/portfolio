3fdeb79958bd3927251a14734941258ffaa33758
florescente <campoflorescente@gmail.com>
2023-06-05 21:39:46 -0300

    feat: add mdx

--- app/blog/[blogId]/page.tsx
+++ app/blog/[blogId]/page.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import path from 'path'
+import { sync } from 'glob'
+
+export const dynamicParams = false
 
 interface Posts {
   params: {
@@ -6,12 +10,32 @@ interface Posts {
   }
 }
 
-function article({ params }: Posts): React.JSX.Element {
+async function article({ params }: Posts): Promise<React.JSX.Element> {
   const { blogId } = params
+  const Article = React.lazy(
+    async () => await import(`../articles/${blogId}.mdx`)
+  )
   return (
-    <main className="flex justify-center items-center grow">
+    <main className="flex justify-center items-center grow flex-col">
       <div className="text-3xl font-bold">{blogId}</div>
+      <Article />
     </main>
   )
 }
+
+export async function generateStaticParams(): Promise<
+  Array<{
+    slug: string
+  }>
+> {
+  const articlesPath = path.join(process.cwd(), 'app/blog/articles')
+  const paths = sync(`${articlesPath}/*.mdx`)
+  return paths.map((path: any) => {
+    const pathContent = path.split('/')
+    const fileName = pathContent[pathContent.length - 1]
+    const slug = fileName.split('.')[0]
+    return slug
+  })
+}
+
 export default article
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
-import data from './data'
 import Link from 'next/link'
+import path from 'path'
+import { sync } from 'glob'
 
 function blog(): React.JSX.Element {
+  const articlesPath = path.join(process.cwd(), 'app/blog/articles')
+  const paths = sync(`${articlesPath}/*.mdx`)
+
   return (
     <main className="flex flex-col justify-center items-center grow">
       <div className="text-3xl font-bold mb-6">blogs</div>
-      {data?.map((post) => {
+      {paths?.map((path) => {
+        const pathContent = path.split('/')
+        const fileName = pathContent[pathContent.length - 1]
+        const slug = fileName.split('.')[0]
         return (
-          <Link href={'/blog/' + post.title} key={post.title}>
+          <Link href={'/blog/' + slug} key={slug}>
             <div
               className="flex border-2 rounded-lg mb-3 w-96 h-28 justify-center items-center hover:bg-pink"
-              key={post.title}
+              key={slug}
             >
-              {post.title}
+              {slug}
             </div>
           </Link>
         )
